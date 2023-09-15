435f4a9cda5f9beca4e75afe76fc06b4578d8afc
Berend Holtland <berend@road.io>
2023-09-15 16:44:04 +0200

    Fix available status notification

--- src/lib/ChargeStation/eventHandlers/ocpp-16/send-status-notification-available.js
+++ src/lib/ChargeStation/eventHandlers/ocpp-16/send-status-notification-available.js
@@ -7,9 +7,20 @@ export default async function sendStatusNotificationAvailable(
 ) {
   await sleep(200);
 
-  await chargepoint.sendCommand('StatusNotification', {
-    connectorId: session.connectorId,
-    errorCode: 'NoError',
-    status: 'Available',
-  });
+  if (session?.connectorId) {
+    await chargepoint.sendCommand('StatusNotification', {
+      connectorId: session.connectorId,
+      errorCode: 'NoError',
+      status: 'Available',
+    });
+    return;
+  }
+
+  for (let i = 0; i < chargepoint.configuration.NumberOfConnectors; i++) {
+    await chargepoint.sendCommand('StatusNotification', {
+      connectorId: i + 1,
+      errorCode: 'NoError',
+      status: 'Available',
+    });
+  }
 }
