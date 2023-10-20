5006f3ec32c2fcdc0bd32c2f8b91966bd8dccfd2
Berend Holtland <berend@road.io>
2023-10-20 13:51:04 +0200

    Convert ifs to switch

--- src/lib/ChargeStation/utils.ts
+++ src/lib/ChargeStation/utils.ts
@@ -17,23 +17,21 @@ export function summarizeCommandParams({
   method,
   params: { idTag, meterStart, meterStop, meterValue, status },
 }: SummarizeCommandParams) {
-  if (method === 'StartTransaction') {
-    return { meterStart: meterStart, uid: idTag };
+  switch (method) {
+    case 'StartTransaction':
+      return { meterStart, uid: idTag };
+    case 'StopTransaction':
+      return { meterStop, uid: idTag };
+    case 'Authorize':
+      return { uid: idTag };
+    case 'StatusNotification':
+      return { status };
+    case 'MeterValues':
+      return {
+        kwh: meterValue ? meterValue[0]?.sampledValue[0]?.value : undefined,
+      };
   }
-  if (method === 'StopTransaction') {
-    return { meterStop: meterStop, uid: idTag };
-  }
-  if (method === 'Authorize') {
-    return { uid: idTag };
-  }
-  if (method === 'StatusNotification') {
-    return { status: status };
-  }
-  if (method === 'MeterValues') {
-    return {
-      kwh: meterValue ? meterValue[0]?.sampledValue[0]?.value : undefined,
-    };
-  }
+
   return null;
 }
 
