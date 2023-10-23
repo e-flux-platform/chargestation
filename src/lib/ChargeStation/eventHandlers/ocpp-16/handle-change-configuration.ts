a01c70ed028b9db7b9a3a297e3aeae3c468d270a
Berend Holtland <berend@road.io>
2023-10-23 15:31:16 +0200

    convert change configuration as well

---
+++ src/lib/ChargeStation/eventHandlers/ocpp-16/handle-change-configuration.ts
@@ -0,0 +1,17 @@
+import { ChargeStationEventHandler } from 'lib/ChargeStation/eventHandlers';
+import { ChangeConfigurationRequest } from 'schemas/ocpp/1.6/ChangeConfiguration';
+
+const handleChangeConfigurationHandler: ChargeStationEventHandler<ChangeConfigurationRequest> =
+  ({ chargepoint, callMessageId, callMessageBody }) => {
+    const { key } = callMessageBody;
+
+    chargepoint.configuration.setVariable(key, callMessageBody);
+
+    const response = {
+      status: 'Accepted',
+    };
+
+    chargepoint.writeCallResult(callMessageId, response);
+  };
+
+export default handleChangeConfigurationHandler;
