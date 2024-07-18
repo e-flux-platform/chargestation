b7b8fe199cb6fc2a258ff4f60dd00f92bb8899f2
Nicholas Jones <me@nicholasjon.es>
2024-07-18 16:45:42 +0100

    Add ADS-TEC model

---
+++ src/lib/ChargeStation/configurations/ads-tec-ocpp-16.ts
@@ -0,0 +1,21 @@
+import { EventTypes as e } from '../eventHandlers/event-types';
+import DefaultOCPP16 from 'lib/ChargeStation/configurations/default-ocpp-16';
+import sendStartTransaction from 'lib/ChargeStation/eventHandlers/ocpp-16/send-start-transaction';
+import { ChargeStationEventHandler } from 'lib/ChargeStation/eventHandlers';
+import sendAuthorize from 'lib/ChargeStation/eventHandlers/ocpp-16/send-authorize';
+import { AuthorizationType } from 'lib/settings';
+
+const initiateSession: ChargeStationEventHandler = async (params) => {
+  const { session, chargepoint } = params;
+  if (session.options.authorizationType === AuthorizationType.CreditCard) {
+    session.options.uid = chargepoint.configuration.getVariableValue('CreditIdToken') as string;
+    await sendStartTransaction(params);
+  } else {
+    await sendAuthorize(params);
+  }
+}
+
+export default {
+  ...DefaultOCPP16,
+  [e.SessionStartInitiated]: [initiateSession],
+};
--- src/lib/ChargeStation/configurations/index.js
+++ src/lib/ChargeStation/configurations/index.js
@@ -1,13 +1,15 @@
 import DefaultOCPP16 from './default-ocpp-16';
 import DefaultOCPP20 from './default-ocpp-20';
 import AlpitronicCCVOCPP16 from './alpitronic-ccv-ocpp-16';
-import SichargeOCPP16 from './sicharge';
+import SichargeOCPP16 from './sicharge-ocpp-16';
+import AdsTecOCPP16 from './ads-tec-ocpp-16';
 
 const options = {
   'default-ocpp1.6': DefaultOCPP16,
   'default-ocpp2.0.1': DefaultOCPP20,
   'ccv-alpitronic-ocpp1.6': AlpitronicCCVOCPP16,
   'sicharge-ocpp1.6': SichargeOCPP16,
+  'ads-tec-ocpp1.6': AdsTecOCPP16,
 };
 
 export function getOCPPConfigurationOptions() {
