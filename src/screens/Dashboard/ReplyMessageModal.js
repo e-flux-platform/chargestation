e17914ed932044342373e5da5dffc59cdabb2a26
Lucas Vieira <lucas.vieira@road.io>
2025-01-31 14:15:14 +0100

    show error in case it cannot parse json

--- src/screens/Dashboard/ReplyMessageModal.js
+++ src/screens/Dashboard/ReplyMessageModal.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { Modal, Button, Form, Divider } from 'semantic';
+import { Modal, Button, Form, Divider, Message } from 'semantic';
 
 import modal from 'helpers/modal';
 
@@ -8,12 +8,19 @@ import modal from 'helpers/modal';
 export default class ReplyMessageModal extends React.Component {
   state = {
     payload: null,
+    error: null,
   };
 
   onSubmit = () => {
-    const payload = this.state.payload ? JSON.parse(this.state.payload) : '';
-    this.props.onSave({ payload });
-    this.props.close();
+    try {
+      this.setState({ error: null });
+      const payload = this.state.payload ? JSON.parse(this.state.payload) : '';
+      this.props.onSave({ payload });
+      this.props.close();
+    } catch (e) {
+      this.setState({ error: e.message });
+      console.error(e);
+    }
   };
 
   render() {
@@ -40,13 +47,14 @@ export default class ReplyMessageModal extends React.Component {
           <Divider />
           <Form onSubmit={this.onSubmit} id="reply-message-form">
             <Form.TextArea
-              label="Response"
+              label="Response (JSON format)"
               name="payload"
               value={this.state.payload}
               onChange={(e) => this.setState({ payload: e.target.value })}
             />
             <Divider hidden />
           </Form>
+          {this.state.error && <Message error>{this.state.error}</Message>}
         </Modal.Content>
         <Modal.Actions>
           <Button primary form="reply-message-form" content="Reply" />
--- src/screens/Dashboard/SendMessageModal.js
+++ src/screens/Dashboard/SendMessageModal.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { Modal, Button, Form, Divider } from 'semantic';
+import { Modal, Button, Form, Divider, Message } from 'semantic';
 
 import modal from 'helpers/modal';
 
@@ -9,12 +9,19 @@ export default class SendMessageModal extends React.Component {
   state = {
     action: 'Heartbeat',
     payload: '{}',
+    error: null,
   };
 
   onSubmit = () => {
-    const payload = this.state.payload ? JSON.parse(this.state.payload) : '';
-    this.props.onSave({ action: this.state.action, payload });
-    this.props.close();
+    try {
+      this.setState({ error: null });
+      const payload = this.state.payload ? JSON.parse(this.state.payload) : '';
+      this.props.onSave({ action: this.state.action, payload });
+      this.props.close();
+    } catch (e) {
+      this.setState({ error: e.message });
+      console.error(e);
+    }
   };
 
   render() {
@@ -31,13 +38,14 @@ export default class SendMessageModal extends React.Component {
               onChange={(e) => this.setState({ action: e.target.value })}
             />
             <Form.TextArea
-              label="Payload"
+              label="Payload (JSON format)"
               name="payload"
               value={this.state.payload}
               onChange={(e) => this.setState({ payload: e.target.value })}
             />
             <Divider hidden />
           </Form>
+          {this.state.error && <Message error>{this.state.error}</Message>}
         </Modal.Content>
         <Modal.Actions>
           <Button primary form="send-message-form" content="Send" />
